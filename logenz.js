01b7a08c211c9e4da06f6425832641ba1ca16c0a
Nasus20202 <krzysiek.com.pl@wp.pl>
2020-04-18 13:44:43 +0200

    Format changed

--- logenz.js
+++ logenz.js
@@ -11,14 +11,14 @@ logenz.on('guildMemberAdd', member =>{
     const channel = member.guild.systemChannel;
     if(!channel) return;
     console.log(`Hello, ${member}`);
-    channel.send(`Welcome to the server, ${member}`);
+    channel.send(`>Welcome to the server, ${member}`);
 });
 
 logenz.on('guildMemberRemove', member =>{
     const channel = member.guild.systemChannel;
     if(!channel) return;
     console.log(`Goodbye, ${member}`);
-    channel.send(`Goodbye, ${member}`);
+    channel.send(`>Goodbye, ${member}`);
 });
 
 logenz.login(process.env.TOKEN);
