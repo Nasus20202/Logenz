95007d4af2aedf7a72ca1b837b3d3de9f1d21eb2
Nasus20202 <krzysiek.com.pl@wp.pl>
2020-04-18 12:18:59 +0200

    Bod greets and says goodbye!

--- logenz.js
+++ logenz.js
@@ -7,4 +7,18 @@ logenz.on('ready', () => {
     console.log(`Just logged into Discord as ${logenz.user.tag}!`);
 });
 
+logenz.on('guildMemberAdd', member =>{
+    const channel = member.guild.channels.cache.find(ch => ch.name === /*'ogólny'*/ 'test-site');
+    if(!channel) return;
+    console.log(`Hello, ${member}`);
+    channel.send(`Welcome to the server, ${member}`);
+});
+
+logenz.on('guildMemberRemove', member =>{
+    const channel = member.guild.channels.cache.find(ch => ch.name === /*'ogólny'*/ 'test-site');
+    if(!channel) return;
+    console.log(`Goodbye, ${member}`);
+    channel.send(`Goodbye, ${member}`);
+});
+
 logenz.login(process.env.TOKEN);
